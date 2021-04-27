fc07b31e3ca00596643fdab5a45af1c2131843de
Omar M Juarez <omarmju906p@hotmail.com>
2021-04-26 23:23:51 -0500

    Se corrige error para eliminar un elemento por ID.

--- routers/movies.js
+++ routers/movies.js
@@ -45,7 +45,7 @@ function moviesAPI (app) {
             const createMovie = await moviesService.createMovie({ movie });
             res.status(201).json({
                 data: createMovie,
-                message: "movie created"
+                message: "Movie created"
             });
         } catch (error) {
             next(error);
@@ -70,13 +70,13 @@ function moviesAPI (app) {
 
     // Método para borrar una película.
     router.delete("/:movieId", async function (req, res, next) {
-        const { movieId } = req.param;
+        const { movieId } = req.params;
 
         try {
             const deleteMovie = await moviesService.deleteMovie({ movieId });
             res.status(200).json({
                 data: deleteMovie,
-                message: "movie deleted"
+                message: "Movie deleted"
             });
         } catch (error) {
             next(error);
