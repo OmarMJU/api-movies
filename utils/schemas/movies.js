a30c9aa2d349d2487cef7e6195633b2bf15593da
Omar M Juarez <omarmju906p@hotmail.com>
2021-05-05 23:15:34 -0500

    Se corrigen los schemas.

--- utils/schemas/movies.js
+++ utils/schemas/movies.js
@@ -5,7 +5,7 @@ const movieIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}/);
 const movieTitleSchema = joi.string().max(80);
 const movieYearSchema = joi.number().min(1888).max(2077);
 const movieCoverSchema = joi.string().uri();
-const movieDescriptionSchema = joi.string.max(300);
+const movieDescriptionSchema = joi.string().max(300);
 const movieDurationSchema = joi.number().min(1).max(300);
 const movieContentRatingSchema = joi.string().max(5);
 const movieSourceSchema = joi.string().uri();
@@ -15,7 +15,7 @@ const createMovieSchema = {
     title: movieTitleSchema.required(),
     year: movieYearSchema.required(),
     cover: movieCoverSchema.required(),
-    description: movieDescriptionSchema.require(),
+    description: movieDescriptionSchema.required(),
     duration: movieDurationSchema.required(),
     contentRating: movieContentRatingSchema.required(),
     source: movieSourceSchema.required(),
