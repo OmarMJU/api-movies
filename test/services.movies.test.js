5ad93f0699c8303118065b91d2fe967df3cc2571
Omar M Juarez <omarmju906p@hotmail.com>
2021-05-27 21:46:40 -0500

    Se agrega TEST para la lista de peliculas.

--- test/services.movies.test.js
+++ test/services.movies.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const proxyquire = require("proxyquire");
 const { getAllStub, MongoLibMock } = require("../utils/mocks/mongoLib");
-// const { moviesMock } = require("../utils/mocks/movies");
+const { moviesMock } = require("../utils/mocks/movies");
 
 describe("services - movies", function() {
     const MoviesServices = proxyquire("../service/movies", {
@@ -14,6 +14,12 @@ describe("services - movies", function() {
         it("should call the getall MongoLib  method", async function() {
             await moviesServices.getMovies({});
             assert.strictEqual(getAllStub.called, true);
+        });
+
+        it("should return an array of movies", async function() {
+            const result = await moviesServices.getMovies({});
+            const expected = moviesMock;
+            assert.deepEqual(result, expected);
         });
     });
 });
