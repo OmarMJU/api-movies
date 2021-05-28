3039a131b1e75535946d6c75893c141f3d7b9f86
Omar M Juarez <omarmju906p@hotmail.com>
2021-05-27 22:52:13 -0500

    Se agregan TEST para las utilidades con TDD.

---
+++ test/utils.buildMessage.test.js
@@ -0,0 +1,20 @@
+const assert = require("assert");
+const buildMessage = require("../utils/buildMessage");
+
+describe.only("utils - buildMessage", function() {
+    describe("wheb recives an entity and an action", function() {
+        it("should return the respective message", function() {
+            const result = buildMessage("movie", "create");
+            const expected = "movie created";
+            assert.strictEqual(result, expected);
+        });
+    });
+
+    describe("recives an entity and action is list", function() {
+        it("it should returns the respective message with entity is a list", function() {
+            const result = buildMessage("movie", "list");
+            const expected = "movies listed";
+            assert.strictEqual(result, expected);
+        });
+    });
+});
